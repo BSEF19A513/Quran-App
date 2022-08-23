cbc4545648d3ade1e00b4a4602554bb39809b417
Hassan Zahid <bsef19a513@pucit.edu.pk>
2022-08-23 20:44:45 +0500

    first commit

---
+++ app/src/main/java/com/example/quranapp/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.quranapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+    ListView surahNamesView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        surahNamesView = findViewById(R.id.surahNamesView);
+        QDH obj = new QDH();
+        String[] surahNames=obj.englishSurahNames;
+
+        int[] ssp = obj.SSP;
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1,surahNames);
+        surahNamesView.setAdapter(arrayAdapter);
+        surahNamesView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                int startIndex = ssp[i];
+                int endIndex = ssp[i+1];
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                intent.putExtra("startIndex",startIndex);
+                intent.putExtra("endIndex",endIndex);
+                startActivity(intent);
+//                Log.d("======", "onItemClick: "+startIndex+" "+endIndex);
+            }
+        });
+
+    }
+}
